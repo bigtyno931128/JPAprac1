bf49f264be2b550712571f7ecd4f1e5a0eadf328
bigtyno <bigtyno@naver.com>
2022-05-30 09:34:11 +0900

    Initial commit

---
+++ src/test/java/jpaprac/jpaprac/JpapracApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaprac.jpaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpapracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
